0f49dc34ac03b6ec65f416b09c6efce8a48db543
ShadowKingTools <juno009ox3@gmail.com>
2026-01-08 11:28:21 -0800

    Add SessionPrep starter script

---
+++ SessionPrep.ps1
@@ -0,0 +1,11 @@
+# SessionPrep.ps1
+# ShadowKingAutomationPack - starter script
+
+Write-Host "SessionPrep: starting..." -ForegroundColor Cyan
+
+# Example: show date/time + machine info
+Write-Host ("Time: " + (Get-Date))
+Write-Host ("Computer: " + $env:COMPUTERNAME)
+Write-Host ("User: " + $env:USERNAME)
+
+Write-Host "SessionPrep: done." -ForegroundColor Green
