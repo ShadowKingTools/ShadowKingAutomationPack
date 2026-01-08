99cbbc3ab524981a69049f252fd8c0d675eab5cf
ShadowKingTools <juno009ox3@gmail.com>
2026-01-08 12:21:44 -0800

    Add LaunchStack starter script

---
+++ LaunchStack.ps1
@@ -0,0 +1,12 @@
+# LaunchStack.ps1
+# ShadowKingAutomationPack - starter script
+
+Write-Host "LaunchStack: starting..." -ForegroundColor Cyan
+
+# TODO: add your real apps/games here
+# Examples (uncomment and edit later):
+# Start-Process "steam://rungameid/1091500"   # Cyberpunk (example)
+# Start-Process "C:\Program Files\Google\Chrome\Application\chrome.exe"
+# Start-Process "C:\Windows\System32\notepad.exe"
+
+Write-Host "LaunchStack: done (no apps launched yet)." -ForegroundColor Yellow
